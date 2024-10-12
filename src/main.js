b4be306e0f967806e43ad4adb83bf11ba8dfbfeb
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2024-10-12 19:35:44 +0100

    check img

--- src/main.js
+++ src/main.js
@@ -1,62 +1,67 @@
 import "./index.css";
 
-// Get the button
-const mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-
-const scrollFunction = () => {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.classList.remove("hidden");
-  } else {
-    mybutton.classList.add("hidden");
-  }
-};
-const backToTop = () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
-
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-window.addEventListener("scroll", scrollFunction);
-
-import { Collapse, Dropdown, initTWE } from "../tw-elements";
+import { Collapse, Dropdown, initTWE } from "/tw-elements";
 
 initTWE({ Collapse, Dropdown });
 
-const text = "Taste the Quality, Cherish the Affordability";
-const typewriterElement = document.getElementById("typewriter");
-let index = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the button
+  const mybutton = document.getElementById("btn-back-to-top");
 
-function type() {
-  if (index < text.length) {
-    typewriterElement.textContent += text.charAt(index);
-    index++;
-    setTimeout(type, 100); // Adjust typing speed here
-  } else {
-    // Optional: Add a blinking cursor effect after typing is done
-    typewriterElement.classList.add("animate-blink");
+  // When the user scrolls down 20px from the top of the document, show the button
+
+  const scrollFunction = () => {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.classList.remove("hidden");
+    } else {
+      mybutton.classList.add("hidden");
+    }
+  };
+  const backToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+
+  window.addEventListener("scroll", scrollFunction);
+
+  const text = "Taste the Quality, Cherish the Affordability";
+  const typewriterElement = document.getElementById("typewriter");
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      typewriterElement.textContent += text.charAt(index);
+      index++;
+      setTimeout(type, 100); // Adjust typing speed here
+    } else {
+      // Optional: Add a blinking cursor effect after typing is done
+      typewriterElement.classList.add("animate-blink");
+    }
   }
-}
-type();
+  type();
 
-const sr = ScrollReveal();
-sr.reveal(".reveal", {
-  delay: 200,
-  interval: 600,
-  distance: "20px",
-  easing: "ease-in",
-  opacity: 0,
-  reset: false,
-  duration: 800,
-});
-sr.reveal(".scaleUp", {
-  scale: 0.85,
-  delay: 200,
-  reset: false,
-  interval: 600,
-  distance: "20px",
-  easing: "ease-in",
-  opacity: 0,
+  const sr = ScrollReveal();
+  sr.reveal(".reveal", {
+    delay: 200,
+    interval: 600,
+    distance: "20px",
+    easing: "ease-in",
+    opacity: 0,
+    reset: false,
+    duration: 800,
+  });
+  sr.reveal(".scaleUp", {
+    scale: 0.85,
+    delay: 200,
+    reset: false,
+    interval: 600,
+    distance: "20px",
+    easing: "ease-in",
+    opacity: 0,
+  });
 });
--- src/products.js
+++ src/products.js
@@ -1,6 +1,6 @@
 const fetchProducts = async () => {
   try {
-    const response = await fetch("products.json");
+    const response = await fetch("./src/products.json");
 
     if (!response.ok) {
       throw new Error("Couldnt Get Product.json");
@@ -15,33 +15,37 @@ const fetchProducts = async () => {
 };
 
 const displayContent = (products) => {
-  const productContainer = document.getElementById("product-container");
+  try {
+    const productContainer = document.getElementById("product-container");
 
-  products.forEach((product) => {
-    const productCard = document.createElement("div");
-    productCard.setAttribute("data-id", product.id);
-    productCard.innerHTML = `
-    <article class="scaleUp relative flex flex-col overflow-hidden rounded-lg border">
-            <div class="aspect-square overflow-hidden">
-              <img class="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="${product.image}" alt="" />
-            </div>
-            <div id="availBtn" class="absolute top-0 m-2 rounded-full bg-white">
-              <p  class=" rounded-full bg-emerald-500 p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">Available</p>
-            </div>
-            <div class="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-              <div class="mb-2 flex">
-                <p class="mr-3 text-2xl font-bold">${product.price}</p>
-                <del class="text-xs text-red-400"> -10% </del> 
+    products.forEach((product) => {
+      const productCard = document.createElement("div");
+      productCard.setAttribute("data-id", product.id);
+      productCard.innerHTML = `
+      <article class="scaleUp relative flex flex-col overflow-hidden rounded-lg border">
+              <div class="aspect-square overflow-hidden">
+                <img class="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="${product.image}" alt="" />
               </div>
-              <h3 class="mb-2 text-lg md:text-3xl font-bold text-gray-500">${product.name}</h3>
-            </div>
-            <button data-twe-ripple-init  class="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-              <div class="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-[#E65C00] group-hover:text-white">Add</div>
-              <div class="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-[#fa8025] group-hover:text-white">+</div>
-            </button> 
-          </article>`;
+              <div id="availBtn" class="absolute top-0 m-2 rounded-full bg-white">
+                <p  class=" rounded-full bg-emerald-500 p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">Available</p>
+              </div>
+              <div class="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
+                <div class="mb-2 flex">
+                  <p class="mr-3 text-2xl font-bold">${product.price}</p>
+                  <del class="text-xs text-red-400"> -10% </del> 
+                </div>
+                <h3 class="mb-2 text-lg md:text-3xl font-bold text-gray-500">${product.name}</h3>
+              </div>
+              <button data-twe-ripple-init  class="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
+                <div class="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-[#E65C00] group-hover:text-white">Add</div>
+                <div class="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-[#fa8025] group-hover:text-white">+</div>
+              </button> 
+            </article>`;
 
-    productContainer.appendChild(productCard);
-  });
+      productContainer.appendChild(productCard);
+    });
+  } catch (error) {
+    console.log("check here", error.message);
+  }
 };
 fetchProducts();
