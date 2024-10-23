b0fb9eb152ef05547105b740768fc879263ef3be
Netochukwu Codes <161637474+Netocodes@users.noreply.github.com>
2024-10-23 10:47:33 +0100

    added base confige for better routing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/Aunty-Ozy-Foodies/",
+// export default defineConfig({
+//   // base: "/Aunty-Ozy-Foodies/",
+//   server: {
+//     sourcemap: false,
+
+//     mime: {
+//       // Set the MIME type for .js files to application/javascript
+//       js: "application/javascript",
+//     },
+//   },
+//   plugins: [
+//     tailwindcss(), // Add Tailwind CSS as a plugin
+//   ],
+// });
+
+export default defineConfig(({ mode }) => ({
+  base: mode === "github" ? "/Aunty-Ozy-Foodies/" : "./",
   server: {
     sourcemap: false,
-
     mime: {
       // Set the MIME type for .js files to application/javascript
       js: "application/javascript",
@@ -14,4 +28,4 @@ export default defineConfig({
   plugins: [
     tailwindcss(), // Add Tailwind CSS as a plugin
   ],
-});
+}));
